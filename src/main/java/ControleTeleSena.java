e2fa88bb5c0eb8ac2d73df3ef3f419da4c1d32a7
pedrocquadros <112824554+pedrocquadros@users.noreply.github.com>
2022-11-16 18:22:21 -0300

    16/11

--- src/main/java/ControleTeleSena.java
+++ src/main/java/ControleTeleSena.java
@@ -1,6 +1,9 @@
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 @Data
 public class ControleTeleSena {
@@ -11,7 +14,11 @@ public class ControleTeleSena {
     private TeleSena[] premiado = new TeleSena[1];
     private int vendas = 0;
 
-    private int dinheiro = 0;
+    private double dinheiro = 0;
+
+    private int vencedores = 0;
+
+    private boolean fim = false;
 
     public ControleTeleSena() {
         this.pessoa = pessoa;
@@ -25,53 +32,88 @@ public class ControleTeleSena {
         premiado[0] = new TeleSena(10);
 
 
+
         for (int i = 0; i < 20; i++) {
             int quantidadeTelesenasPessoa = (int) (Math.random() * 15 + 1);
             pessoa[i] = new Pessoa(nomePessoas[i], quantidadeTelesenasPessoa);
 
         }
 
+
     }
+
+
+
+
+
+
 
     public void sortear() {
-        for (int i = 0; i < premiado[0].getT1().length; i++) {
-            premiado[0].getT1()[i] = (int) (Math.random() * 60 + 1);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Random randomGenerator = new Random();
+        while (numbers.size() < 60) {
+            int random = randomGenerator.nextInt(60);
+            if (!numbers.contains(random)) {
+                numbers.add(random);
+            }
         }
-        System.out.println("A telesena premiada é: ");
-        System.out.println(java.util.Arrays.toString(premiado[0].getT1()));
+            for (int i = 0; i < premiado[0].getT1().length; i++) {
+                premiado[0].getT1()[i] = numbers.get(i);
 
-    }
+            }
+            System.out.println("A telesena premiada é: ");
+            System.out.println(java.util.Arrays.toString(premiado[0].getT1()));
+
+        }
+
 
     public void comprarTeleSena() {
-        for (int i = 0; i < pessoa.length; i++) {
-            for (int c = 0; c < pessoa[c].getTelesenas().length; c++) {
-                vendas = vendas + 1;
-                dinheiro = dinheiro + 10;
+        while (fim == false) {
+            for (int i = 0; i < pessoa.length; i++) {
+                for (int c = 0; c < pessoa[c].getTelesenas().length; c++) {
+                    vendas = vendas + 1;
+                    dinheiro = dinheiro + tele[0].getValorVenda();
 
-                for (int cont = 0; cont < tele.length; cont++) {
-                    for (int q = 0; q < tele[cont].getT1().length; q++) {
-                        tele[cont].getT1()[q] = (int) (Math.random() * 60 + 1);
+                    for (int cont = 0; cont < tele.length; cont++) {
+                        for (int q = 0; q < tele[cont].getT1().length; q++) {
+                            tele[cont].getT1()[q] = (int) (Math.random() * 60 + 1);
+
+                        }
+                        for (int q = 0; q < tele[cont].getT2().length; q++) {
+                            tele[cont].getT2()[q] = (int) (Math.random() * 60 + 1);
+
+                        }
+                        System.out.printf(java.util.Arrays.toString(tele[cont].getT1()));
+                        System.out.printf(java.util.Arrays.toString(tele[cont].getT2()));
+
+                        if (Arrays.equals(tele[cont].getT1(), premiado[0].getT1()) || Arrays.equals(tele[cont].getT2(), premiado[0].getT1())) {
+                            pessoa[c].setVencedor(true);
+                            setFim(true);
+                            vencedores = vencedores + 1;
+                        }
+
                     }
-                    for (int q = 0; q < tele[cont].getT2().length; q++) {
-                        tele[cont].getT2()[q] = (int) (Math.random() * 60 + 1);
-
-                    }
-                    System.out.printf(java.util.Arrays.toString(tele[cont].getT1()));
-                    System.out.printf(java.util.Arrays.toString(tele[cont].getT2()));
-
-                    if (Arrays.equals(tele[cont].getT1(), tele[cont].getT2())) {
-                        pessoa[c].setVencedor(true);
-                    }
-
                 }
             }
+            System.out.println(vendas);
+            System.out.println(dinheiro);
+            System.out.println(fim);
+
         }
-        System.out.println(vendas);
-        System.out.println(dinheiro);
         for (int i = 0; i < pessoa.length; i++) {
-            if(pessoa[i].isVencedor()){
-                System.out.println(pessoa[i].getNomePessoa());
+            if (pessoa[i].isVencedor()) {
+                System.out.println(pessoa[i].getNomePessoa() + "venceu! ");
+                pessoa[i].setPremio(dinheiro*0.8/vencedores);
             }
         }
+    }
+
+    public void imprimirInfo(){
+        System.out.println("Número de vencedores: " + vencedores);
+        System.out.println("Número de telesenas vendidas: " + vendas);
+        System.out.println("Valor total arrecadado nas vendas: " + dinheiro );
+        System.out.println("Valor do prêmio para cada vencedor: " + dinheiro*0.8/vencedores);
+        System.out.println("Lucro do Silvio Santos: " + dinheiro*0.20);
+
     }
 }
